f2a6e0fb3dc3146951da8329743c0df95a22e582
aryan <contact@aryanaftab.com>
2025-11-23 19:11:09 +0000

    feat!: migrate source from private repo

---
+++ assets/ts/persistent-light-theme-toggle.ts
@@ -0,0 +1,12 @@
+// Script: persistent-light-theme-toggle.ts
+// Copyright (c) 2025 Aryan
+// SPDX-License-Identifier: BSD-3-Clause
+// Version: 1.0.0
+document.addEventListener('DOMContentLoaded', () => {
+  const checkbox = document.querySelector('#light-mode-button') as HTMLInputElement;
+
+  checkbox.checked = localStorage.getItem('light_mode') === 'true';
+  checkbox.addEventListener('change', () => {
+    localStorage.setItem('light_mode', checkbox.checked.toString());
+  });
+});
